5985c3a357a6f88eaa66f3453ffb3e1da0cddeaf
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-02-06 22:03:20 +0300

    Core Project Added

---
+++ Core/DataAcess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAcess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext>
+    {
+    }
+}
