c29131c9ffecfd1edb158933be036fa87eb21388
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-02-06 20:58:00 +0300

    Entity Framework Added to DataAcces and Operations Added to Business

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -8,5 +8,9 @@ namespace Business.Abstract
     public interface IProductService
     {
         List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int categoryId);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
+
+
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -20,5 +20,15 @@ namespace Business.Concrete
         {
             return _productDal.GetAll();
         }
+
+        public List<Product> GetAllByCategoryId(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
     }
 }
