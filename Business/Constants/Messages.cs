d39943dd3abdf9fbc6947f79c9be145f3af9345a
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-02-28 00:55:03 +0300

    DependencyInjections Updated Messages Updated

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -16,11 +16,11 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists = "Aynı isme sahip başka bir ürün var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için ürün eklemesi başarısız oldu";
         public static string AuthorizationDenied;
-        internal static string UserRegistered;
-        internal static User UserNotFound;
-        internal static User PasswordError;
-        internal static string SuccessfulLogin;
-        internal static string UserAlreadyExists;
-        internal static string AccessTokenCreated;
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
