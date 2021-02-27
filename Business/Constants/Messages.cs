9a6e6691df3fe29ce72d3502ec4e1510307e82bd
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-02-28 00:47:47 +0300

    new Services And Dals Added to System new Messages Added Extensions Added to Core SecuredOperation Added to BusinessAspects.Autofac folder in Business Layer

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 15 ürün bulunabilir";
         public static string ProductNameAlreadyExists = "Aynı isme sahip başka bir ürün var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için ürün eklemesi başarısız oldu";
+        public static string AuthorizationDenied;
+        internal static string UserRegistered;
+        internal static User UserNotFound;
+        internal static User PasswordError;
+        internal static string SuccessfulLogin;
+        internal static string UserAlreadyExists;
+        internal static string AccessTokenCreated;
     }
 }
