dabe5b54fb9945c05f8b6457d688b3368dc93334
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-03-03 21:10:24 +0300

    Local Changes

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -36,7 +36,9 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
