671471fea58ea0606d5517787feafc5f8a20df35
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-03-25 16:37:13 +0300

    Completed

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
 using FluentValidation.Results;
@@ -26,29 +26,30 @@ namespace Core.Extensions
             }
             catch (Exception e)
             {
-                await HandleExceptionAsync(httpContext,e);
+                await HandleExceptionAsync(httpContext, e);
             }
         }
 
         private Task HandleExceptionAsync(HttpContext httpContext, Exception e)
         {
             httpContext.Response.ContentType = "application/json";
-            httpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "Internal Server Error";
             IEnumerable<ValidationFailure> errors;
-            if (e.GetType()==typeof(ValidationException))
+            if (e.GetType() == typeof(ValidationException))
             {
                 message = e.Message;
                 errors = ((ValidationException) e).Errors;
-                //httpContext.Response.StatusCode = 400;
+                httpContext.Response.StatusCode = 400;
 
-                return httpContext.Response.WriteAsync(new ValidationErrorDetails()
+                return httpContext.Response.WriteAsync(new ValidationErrorDetails
                 {
                     StatusCode = 400,
                     Message = message,
                     Errors = errors
                 }.ToString());
+
             }
 
             return httpContext.Response.WriteAsync(new ErrorDetails
